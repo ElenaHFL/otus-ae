655f25216d3702e0932a10e68d66218bdef9252f
Averyanova Elena <elenaa@hflabs.ru>
2020-07-27 07:53:43 +0300

    use enum in WebDriverFactory

--- tests-junit/src/main/java/WebDriverFactory.java
+++ tests-junit/src/main/java/WebDriverFactory.java
@@ -13,9 +13,35 @@ public class WebDriverFactory {
 
     // Допустимые имена браузеров
     enum DriverName {
-        CHROME,
-        FIREFOX,
-        IE
+        CHROME{
+            public WebDriver create(String str){
+                WebDriverManager.chromedriver().setup();
+                if (str == null) return new ChromeDriver();
+                else return new ChromeDriver(new ChromeOptions().addArguments(str));
+            }
+        },
+        FIREFOX{
+            public WebDriver create(String str){
+                WebDriverManager.firefoxdriver().setup();
+                if (str == null) return new FirefoxDriver();
+                else return new FirefoxDriver(new FirefoxOptions().addArguments(str));
+            }
+        },
+        IE{
+            public WebDriver create(String str){
+                WebDriverManager.iedriver().setup();
+                // NOTE: шоб заработал, надо выровнить уровень защиты + сделать масштаб 100%
+                if (str == null) return new InternetExplorerDriver();
+                else {
+                    // Например: -Doptions=ignoreZoomSetting (тогда масштаб не важен)
+                    DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+                    capabilities.setCapability(str, true);
+                    return new InternetExplorerDriver(new InternetExplorerOptions().merge(capabilities));
+                }
+            }
+        };
+
+        public abstract WebDriver create(String str);
     }
 
     public static WebDriver create(String webDriverName) {
@@ -23,34 +49,7 @@ public class WebDriverFactory {
     }
 
     public static WebDriver create(String webDriverName, String options) {
-
-         switch (normalizeValue(webDriverName)) {
-            case FIREFOX: {
-                System.out.println("Запускается браузер FIREFOX.");
-                WebDriverManager.firefoxdriver().setup();
-                if (options == null) return new FirefoxDriver();
-                else return new FirefoxDriver(new FirefoxOptions().addArguments(options));
-            }
-            case IE: {
-                System.out.println("Запускается браузер IE.");
-                WebDriverManager.iedriver().setup();
-                // NOTE: шоб заработал, надо выровнить уровень защиты + сделать масштаб 100%
-                if (options == null) return new InternetExplorerDriver();
-                else {
-                    // Например: -Doptions=ignoreZoomSetting
-                    DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-                    capabilities.setCapability(options,true);
-                    return new InternetExplorerDriver(new InternetExplorerOptions().merge(capabilities));
-                }
-            }
-            default: {
-                // По умолчанию CHROME
-                System.out.println("Запускается браузер CHROME.");
-                WebDriverManager.chromedriver().setup();
-                if (options == null) return new ChromeDriver();
-                else return new ChromeDriver(new ChromeOptions().addArguments(options));
-            }
-        }
+        return normalizeValue(webDriverName).create(options);
     }
 
     protected static DriverName normalizeValue(String value) {
